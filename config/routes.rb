5e85e5a8afb335ac4f4d66445e719ef67286ae56
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-03-09 08:02:08 +0000

    [Add] 検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'searches/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  get '/search', to: 'searches#search'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, omly: [:create, :destroy]
